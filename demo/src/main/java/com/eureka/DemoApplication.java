a85d871a556abd2bf708f227936dbc4f35294996
unknown <cogjava3158@iiht.tech>
2022-08-24 21:11:30 +0530

    hhhh

--- demo/src/main/java/com/eureka/DemoApplication.java
+++ demo/src/main/java/com/eureka/DemoApplication.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 @SpringBootApplication
 public class DemoApplication {
-
+//jjjjjjj
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
